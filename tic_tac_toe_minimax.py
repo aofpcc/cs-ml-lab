4ac19846e4b80faf64ba5ebb44ca73b5b2106afb
Bunyarit Puangthamawathanakun <aofpc1@gmail.com>
2020-02-07 13:22:07 +0700

    update tic tac toe

--- tic_tac_toe_minimax.py
+++ tic_tac_toe_minimax.py
@@ -12,6 +12,7 @@ wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
 
 win_count = [0, 0, 0, 0]
 
+
 def divide(currentMove):
     if len(currentMove) > 0:
         return [currentMove[::2], currentMove[1::2]]
@@ -26,6 +27,7 @@ def check_win(divided):
         first_win = all(e in first for e in win)
         second_win = all(e in second for e in win)
         if first_win or second_win:
+            # print(divided)
             return [first_win, second_win]
 
     return [first_win, second_win]
@@ -45,7 +47,7 @@ def check_game_end(currentMove):
 
 
 def find_move(samples, currentMove, currentPlayer):
-    weight = 2 ** ( 9 - len(currentMove))
+    weight = 1
     if len(currentMove) > 0:
         last_move = currentMove[len(currentMove) - 1]
     else:
@@ -54,23 +56,38 @@ def find_move(samples, currentMove, currentPlayer):
     if len(samples) == 0 or e["result"]:
         if e["result"]:
             if e["winner"] - 1 == currentPlayer:
-                return ( last_move, weight)
-            return (last_move, -weight)
+                return last_move, weight
+            return last_move, -weight
         elif len(samples) == 0:
-            return (last_move, 0)
-    max = -1024
+            return last_move, 0
+    candidate = []
+    # print(samples)
     for i in range(len(samples)):
         move = samples.pop(i)
         currentMove.append(move)
         last_move, weight = find_move(samples, currentMove, currentPlayer)
-        if weight > max:
-            max = weight
+        candidate.append((last_move, weight))
         currentMove.pop()
         samples.insert(i, move)
-    return (last_move, max)
+
+    if (len(currentMove) + 1) % 2 == currentPlayer: # max
+        sorted_candidate = sorted(candidate, key=lambda v: v[1], reverse=True)
+    else: # min
+        sorted_candidate = sorted(candidate, key=lambda v: v[1], reverse=False)
+    candidate = list(filter(lambda x: x[1] == sorted_candidate[0][1], candidate))
+    # print(samples)
+    # print(str(currentPlayer) + ": ", end='')
+    # print(str(currentMove) + ": ", end='')
+    # print(candidate)
+    random.shuffle(candidate)
+    move = candidate.pop()
+    # print(move)
+    return move
+
 
 def run_game(samples, currentMove):
     e = check_game_end(currentMove)
+    # print(currentMove)
     if len(samples) == 0 or e["result"]:
         if e["result"]:
             # print(*currentMove, end=" ")
@@ -82,7 +99,7 @@ def run_game(samples, currentMove):
             # print(": !!! No Winner")
             win_count[2] += 1
         win_count[3] += 1
-        return
+        return currentMove
     # random.shuffle(samples)
     # move = samples.pop()
 
@@ -90,13 +107,14 @@ def run_game(samples, currentMove):
     samples.remove(move)
 
     currentMove.append(move)
-    run_game(samples, currentMove)
+    return run_game(samples, currentMove)
     # for i in range(len(samples)):
     #   move = samples.pop(i)
     #   currentMove.append(move)
     #   runGame(samples, currentMove)
     #   currentMove.pop()
     #   samples.insert(i, move)
+
 
 t_start = time.time()
 
@@ -106,8 +124,11 @@ for x in range(1):
     start = time.time()
 
     for i in range(100):
+        print( "Game# " + str(i))
         x = samples.copy()
-        run_game(x, [])
+        # run_game([1, 2, 8, 9], [3, 5, 4, 6, 7])
+        last_move = run_game(x, [])
+        print(last_move)
 
     done = time.time()
     print(win_count)
